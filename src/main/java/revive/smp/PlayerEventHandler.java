1c21bd2d284664026236af13ec456162fb310f51
24Kings <24kingsunite@gmail.com>
2022-04-09 17:09:15 -0700

    added chest spawn on natural death

--- src/main/java/revive/smp/PlayerEventHandler.java
+++ src/main/java/revive/smp/PlayerEventHandler.java
@@ -3,32 +3,45 @@ package revive.smp;
 import net.kyori.adventure.text.Component;
 import org.bukkit.*;
 import org.bukkit.block.Block;
+import org.bukkit.block.Chest;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockPlaceEvent;
 import org.bukkit.event.entity.PlayerDeathEvent;
+import org.bukkit.event.inventory.InventoryCloseEvent;
+import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.SkullMeta;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class PlayerEventHandler implements Listener {
+    List<Location> chests = new ArrayList<>();
+
     @EventHandler
     public void onHeadPlace(BlockPlaceEvent event) {
         if(event.getItemInHand().getType() == Material.PLAYER_HEAD) {
             ItemStack head = event.getItemInHand();
             Block block = event.getBlockPlaced();
+            World world = block.getWorld();
+            Location headLocation = block.getLocation();
             Location location = block.getLocation();
+            location.add(0.5,0.0,0.5); //Sets to middle of block
 
             SkullMeta meta = (SkullMeta) head.getItemMeta();
 
             try {
+                location.add(0.0,3.0,0.0);
                 //Respawn Player
                 Player player = meta.getOwningPlayer().getPlayer();
-                player.teleport(location);
+                world.strikeLightning(location);
+                player.teleport(headLocation);
                 player.setGameMode(GameMode.SURVIVAL);
 
                 //Remove Head
-                location.getBlock().setType(Material.AIR);
+                headLocation.getBlock().setType(Material.AIR);
 
                 //Remove head from Inventory
                 Player respawner = event.getPlayer();
@@ -36,8 +49,7 @@ public class PlayerEventHandler implements Listener {
 
             } catch(NullPointerException e)  {
                 event.setCancelled(true);
-                Bukkit.getLogger().warning("Head does not contain valid player!");
-                e.printStackTrace();
+                Bukkit.getLogger().warning("Cannot find player!");
             }
         }
     }
@@ -68,6 +80,52 @@ public class PlayerEventHandler implements Listener {
                 world.dropItemNaturally(location, skull);
             } else {
                 attacker.getInventory().addItem(skull);
+            }
+        }
+    }
+
+    @EventHandler
+    public void onPlayerDeath(PlayerDeathEvent event) {
+        if(event.getEntity().getKiller() == null) {
+            //Place chest at player's death location
+            Player player = event.getPlayer();
+            Block block = event.getPlayer().getLocation().getBlock();
+            block.setType(Material.CHEST);
+            Chest chest = (Chest) block.getState();
+            Inventory inventory = chest.getInventory();
+
+            //Sets dead player into spectator
+            player.setGameMode(GameMode.SPECTATOR);
+
+            //Adds chest Location to list of active chests
+            chests.add(block.getLocation());
+
+            //Creates player skull from dead player
+            ItemStack skull = new ItemStack(Material.PLAYER_HEAD, 1);
+            SkullMeta meta = (SkullMeta) skull.getItemMeta();
+
+            meta.setOwningPlayer(player);
+
+            meta.displayName(Component.text(ChatColor.YELLOW + player.getName() + "'s Head"));
+            skull.setItemMeta(meta);
+
+            //Add skull to chest
+            inventory.addItem(skull);
+        }
+    }
+
+    @EventHandler
+    public void onChestOpen(InventoryCloseEvent event) {
+        Inventory inv = event.getInventory();
+        Location location = inv.getLocation();
+
+        if(inv.isEmpty() && chests.contains(location)) {
+            try {
+                chests.remove(location);
+                event.getInventory().getLocation().getBlock().setType(Material.AIR);
+
+            } catch(NullPointerException e) {
+                e.printStackTrace();
             }
         }
     }
